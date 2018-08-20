04430d17c08a224eff7bb0f7baf03a61b4d5e777
Daniel Lehmberg <daniel.lehmberg@hm.edu>
2018-08-20 10:49:26 +0200

    make creating/removing scenario separater

--- try_outs/scenario.py
+++ try_outs/scenario.py
@@ -3,6 +3,7 @@
 # TODO: """ << INCLUDE DOCSTRING (one-line or multi-line) >> """
 
 import glob
+import copy
 
 from shutil import copyfile, rmtree
 
@@ -17,7 +18,45 @@ __credits__ = ["n/a"]
 # --------------------------------------------------
 
 
+def remove_scenario(sc_folder):
+    if user_query_yes_no(question=f"Are you sure you want to overwrite the scenario? \n {sc_folder}"):
+        rmtree(sc_folder)
+        return True
+    else:
+        return False
+
+
+def create_scenario(sc_folder, sc_basis_file, model, replace=False):
+    # check the given .scenario file
+    assert os.path.isfile(sc_basis_file), "Filepath to .scenario does not exist"
+    assert sc_basis_file.split(".")[-1] == "scenario", "File has to be a VADERE '*.scenario' file"
+
+    # Check if scenario already exists
+    if replace and os.path.exists(sc_folder):
+        if not remove_scenario(sc_folder):
+            print("Aborting to create a new scenario.")
+            return
+
+    # Create new scneario folder
+    os.mkdir(sc_folder)
+
+    # FILL IN THE FILES IN THE NEW SCENARIO:
+
+    # copy the scenario file to the new folder
+    filename = os.path.basename(sc_basis_file)
+    copyfile(sc_basis_file, os.path.join(sc_folder, filename))
+
+    # Create and store the configuration file to the new folder
+    cfg = copy.deepcopy(DEFAULT_SC_CONFIG)
+    cfg["model"] = model
+
+    with open(os.path.join(sc_folder, "sc_config.json"), 'w') as outfile:
+        json.dump(cfg, outfile, indent=4)
+
+
 class ScenarioManager(object):
+
+    # TODO: make scenario creation in an extra class -- this file should *only* manage scenarios
 
     def __init__(self, scenario_path=None):
         if scenario_path is None:
@@ -41,7 +80,6 @@ class ScenarioManager(object):
 
         return ScenarioManager(path)
 
-
     @classmethod
     def setscpath(cls, scpath):
         return ScenarioManager(scenario_path=scpath)
@@ -64,61 +102,14 @@ class ScenarioManager(object):
                 paths_with_scenario.append(p)
         return paths_with_scenario
 
-    def _check_double_sc_names(self):
-        pass
-
-    def _fill_files_new_sc(self, scenario_fp, sc_folder, sc_config):
-
-        # 1. the .scenario file
-        assert os.path.isfile(scenario_fp), "Filepath to .scenario does not exist"
-        assert scenario_fp.split(".")[-1] == "scenario", "File has to be a VADERE '*.scenario' file"
-
-        filename = os.path.basename(scenario_fp)
-        copyfile(scenario_fp, os.path.join(sc_folder, filename))
-
-        # 2. the config file
-        with open(os.path.join(sc_folder, "sc_config.json"), 'w') as outfile:
-            json.dump(sc_config, outfile, indent=4)
-
-    def _create_sc_config(self, model):
-        default_cfg = DEFAULT_SC_CONFIG
-        default_cfg["model"] = model
-        return default_cfg
-
-    def _remove_scenario(self, folder_path):
-        if user_query_yes_no(question=f"Are you sure you want to overwrite the scenario? \n {folder_path}"):
-            rmtree(folder_path)
-            return True
-        else:
-            return False
-
     def get_scfolder_path(self, scname):
         return os.path.join(self._scpath, scname)  # TODO: fix, after merged code from uni
 
-    def get_scvadere_folder(self, scname): # TODO: define 'vadere_scenarios' somehwere in a variable
+    def get_scvadere_folder(self, scname):  # TODO: define 'vadere_scenarios' somehwere in a variable
         rel_path = os.path.join(self.get_scfolder_path(scname), "vadere_scenarios")
         return os.path.abspath(rel_path)
 
-    def add_new_scenario(self, scenario_fp, scname, model, replace=False):
-
-        # = parent folder of the scenario, where all scenarios are stored
-        # target_sc_path = self._sc_path[0]
-
-        scfolder = os.path.join(self._scpath, scname)
-
-        if replace and os.path.exists(scfolder):
-            if not self._remove_scenario(scfolder):
-                print("Aborting to create a new scenario.")
-                return
-
-        assert scname not in self._get_all_sc_names(), f"Scenario {scname} exists already in: \n " \
-                                                              f"{self._scpath}"
-        os.mkdir(scfolder)
-
-        sc_config = self._create_sc_config(model=model)
-        self._fill_files_new_sc(scenario_fp, scfolder, sc_config)
-
-    def get_vadere_scbasis_file(self, sc_name):
+    def get_basis_file(self, sc_name):
         sc_files = glob.glob(os.path.join(self._scpath, sc_name, "*.scenario"))
         assert len(sc_files) == 1, "Multiple .scenario files found..."
 
@@ -129,10 +120,4 @@ class ScenarioManager(object):
 
 
 if __name__ == "__main__":
-    sm = ScenarioManager()
-
-    #scfp = os.path.join(SRC_PATH, "basic_1_chicken_osm1.scenario")
-    #sm.add_new_scenario(scfp, "chicken", "vadere", replace=True)
-
-    print(sm.get_scenario_file("chicken")["processWriters"])
-
+    pass
