59d51553f646211a90ef0b41e30a3d2006e95ad2
Daniel Lehmberg <daniel.lehmberg@hm.edu>
2019-01-17 19:09:25 +0100

    WIP: integration of new way to get data

--- suqc/query.py
+++ suqc/query.py
@@ -5,12 +5,15 @@
 import subprocess
 import multiprocessing
 
-from suqc.qoi import QoIProcessor, PedestrianEvacuationTimeProcessor, AreaDensityVoronoiProcessor
+import pandas as pd
+
+from suqc.qoi import QuantityOfInterest
 from suqc.configuration import EnvironmentManager
 from suqc.parameter.sampling import ParameterVariation, FullGridSampling, RandomSampling, BoxSamplingUlamMethod
 from suqc.parameter.postchanges import ScenarioChanges
 from suqc.parameter.create import VadereScenarioCreation
-from suqc.resultformat import ParameterResult
+
+from typing import List
 
 # --------------------------------------------------
 # people who contributed code
@@ -22,18 +25,14 @@ __credits__ = ["n/a"]
 
 class Query(object):
 
-    def __init__(self, env_man: EnvironmentManager, par_var: ParameterVariation, qoi: QoIProcessor,
+    def __init__(self, env_man: EnvironmentManager, par_var: ParameterVariation, qoi: QuantityOfInterest,
                  sc_change: ScenarioChanges=None):
-
         self._env_man = env_man
-        self._par_var = par_var
         self._qoi = qoi
         self._sc_change = sc_change
-        self._result_df = None
 
-    @property
-    def result(self):
-        return self._result_df
+        self.par_var = par_var
+        self.result = None
 
     def _run_vadere_simulation(self, scenario_fp, output_path):
         model_path = self._env_man.get_model_path()
@@ -42,8 +41,21 @@ class Query(object):
     def _single_query(self, kwargs):
         output_path = self._env_man.get_output_path(kwargs["par_id"], create=True)
         self._run_vadere_simulation(kwargs["scenario_path"], output_path)
-        result = self._qoi.read_and_extract_qoi(kwargs["par_id"], output_path)
+        result = self._qoi.read_and_extract_qois(kwargs["par_id"], output_path)
         return result  # because of the multi-processor, don't try to already add the results here to _results_df
+
+    def _finalize_results(self, results: List[dict]):
+        filenames = results[0].keys()  # it is assumed that the the keys for all elements in results are the same!
+
+        final_results = dict()
+
+        for f in filenames:
+            collected_data = [result_element[f] for result_element in results]
+            collected_data = pd.concat(collected_data, axis=0)
+
+            final_results[f] = collected_data
+
+        return final_results
 
     def _sp_query(self, query_list):
         # enumerate returns tuple(par_id, scenario filepath) see ParameterVariation.generate_vadere_scenarios and
@@ -51,18 +63,18 @@ class Query(object):
         results = list()
         for arg in query_list:
             results.append(self._single_query(arg))
-        self._result_df = ParameterResult.concat_parameter_results(results)
+        self.result = self._finalize_results(results)
 
     def _mp_query(self, query_list, njobs):
         pool = multiprocessing.Pool(processes=njobs)
         results = pool.map(self._single_query, query_list)
-        self._result_df = ParameterResult.concat_parameter_results(results)
+        self.result = self._finalize_results(results)
 
     def run(self, njobs: int=-1):
 
-        assert njobs != 0, "njobs cannot be zero"
+        assert njobs != 0 or njobs < -1, "njobs cannot be zero or smaller than -1"
 
-        nr_simulations = self._par_var.points.shape[0]  # nr of rows = nr of parameter settings = #simulations
+        nr_simulations = self.par_var.points.shape[0]  # nr of rows = nr of parameter settings = #simulations
 
         if njobs == -1:
             avail_cpus = multiprocessing.cpu_count()
@@ -70,14 +82,14 @@ class Query(object):
             print(f"INFO: Available cpus: {avail_cpus}. Nr. of simulation {nr_simulations}. "
                   f"Setting to {njobs} processes.")
 
-        vadcreate = VadereScenarioCreation(self._env_man, self._par_var, self._sc_change)
+        vadcreate = VadereScenarioCreation(self._env_man, self.par_var, self._sc_change)
         query_list = vadcreate.generate_vadere_scenarios(njobs)
 
         if njobs == 1:
             self._sp_query(query_list=query_list)
         else:
             self._mp_query(query_list=query_list, njobs=njobs)
-        return self._par_var.points, self.result
+        return self.par_var.points, self.result
 
 
 if __name__ == "__main__":
@@ -85,21 +97,25 @@ if __name__ == "__main__":
     em = EnvironmentManager("corner")
     pv = FullGridSampling()
     pv.add_dict_grid({"speedDistributionStandardDeviation": [0.0, 0.1, 0.2, 0.3], "speedDistributionMean": [1.2, 1.3]})
-    q0 = PedestrianEvacuationTimeProcessor(em)
+    q0 = QuantityOfInterest("evacuationTimes.txt", em)
 
-    Query(em, pv, q0).run(njobs=-1)
+    q = Query(em, pv, q0).run(njobs=1)
+
+    print(q)
+
+
     exit()
 
     #q1 = PedestrianDensityGaussianProcessor(em) # TODO: need to check if qoi-processor is available in basis file!
-
-
-    pv = BoxSamplingUlamMethod()
-    pv.create_grid("speedDistributionStandardDeviation", 0, 0.5, 3, 3)
-
-    sc = ScenarioChanges(apply_default=True)
-
-    q = Query(em, pv, q0, sc).run(njobs=-1)
-    print(q)
-
-    # q = Query(em, pv, AreaDensityVoronoiProcessor(em)).run(njobs=1)
+    #
+    #
+    # pv = BoxSamplingUlamMethod()
+    # pv.create_grid("speedDistributionStandardDeviation", 0, 0.5, 3, 3)
+    #
+    # sc = ScenarioChanges(apply_default=True)
+    #
+    # q = Query(em, pv, q0, sc).run(njobs=-1)
     # print(q)
+    #
+    # # q = Query(em, pv, AreaDensityVoronoiProcessor(em)).run(njobs=1)
+    # # print(q)
