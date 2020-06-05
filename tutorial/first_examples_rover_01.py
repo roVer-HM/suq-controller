3ff758a1ab3dfd59ca007c049782f470902b74bc
Christina <mayr9@hm.edu>
2020-06-05 16:34:53 +0200

    refactored

--- tutorial/first_examples_rover_01.py
+++ tutorial/first_examples_rover_01.py
@@ -16,27 +16,44 @@ run_local = True
 
 if __name__ == "__main__":
 
-    # define roVer simulation
+    output_folder = os.path.join(os.getcwd(), "first_examples_rover_01")
+
+    ## Define the simulation to be used
+    # A rover simulation is defined by an "omnetpp.ini" file and its corresponding directory.
+    # Use following *.ini file:
+
     path2ini = os.path.join(
         os.environ["ROVER_MAIN"], "rover/simulations/simple_detoure_suqc/omnetpp.ini"
-    )  # use this ini-file
+    )
 
-    output_folder = "first_examples_rover_01"
-
-    qoi = [
-        "degree_informed_extract.txt",
-        "poisson_parameter.txt",
-        "time_95_informed.txt",
-    ]  # qoi
+    ## Define parameters and sampling method
+    # parameters
 
     parameter = [
-        Parameter(name="number_of_agents_mean", simulator="dummy", stages=[0.2, 0.3],)
+        Parameter(
+            name="number_of_agents_mean", simulator="dummy", stages=[15, 20],
+        )  # number of agtens to be generated in 100s
     ]
     dependent_parameters = [
         DependentParameter(
+            name="sources.[id==1].distributionParameters",
+            simulator="vadere",
+            equation=lambda args: [(args["number_of_agents_mean"] * 0.01 / 4)],
+        ),
+        DependentParameter(
+            name="sources.[id==2].distributionParameters",
+            simulator="vadere",
+            equation=lambda args: [(args["number_of_agents_mean"] * 0.01 / 4)],
+        ),
+        DependentParameter(
             name="sources.[id==5].distributionParameters",
             simulator="vadere",
-            equation=lambda args: [(args["number_of_agents_mean"])],
+            equation=lambda args: [(args["number_of_agents_mean"] * 0.01 / 4)],
+        ),
+        DependentParameter(
+            name="sources.[id==6].distributionParameters",
+            simulator="vadere",
+            equation=lambda args: [(args["number_of_agents_mean"] * 0.01 / 4)],
         ),
         DependentParameter(name="sim-time-limit", simulator="omnet", equation="180s"),
         DependentParameter(
@@ -49,10 +66,22 @@ if __name__ == "__main__":
         ),
     ]
 
-    reps = [2, 2]
+    # number of repitions for each sample
+    reps = [1, 4]
+
+    # sampling
     par_var = RoverSamplingFullFactorial(
         parameters=parameter, parameters_dependent=dependent_parameters
     ).get_sampling()
+
+    ## Define the quantities of interest (simulation output variables)
+    # Make sure that corresponding post processing methods exist in the run_script2.py file
+
+    qoi = [
+        "degree_informed_extract.txt",
+        "poisson_parameter.txt",
+        "time_95_informed.txt",
+    ]
 
     setup = CoupledDictVariation(
         ini_path=path2ini,
@@ -74,8 +103,14 @@ if __name__ == "__main__":
             "Please add ROVER_MAIN to your system variables to run a rover simulation."
         )
 
-    summary = output_folder + "_df"
+    ## Run simulations
+    if run_local:
+        par_var, data = setup.run(4)
+    else:
+        par_var, data = setup.remote(-1)
 
+    # Save results
+    summary = output_folder + "_df"
     if os.path.exists(summary):
         shutil.rmtree(summary)
 
@@ -83,11 +118,6 @@ if __name__ == "__main__":
 
     env_man_info = setup.get_env_man_info()
     env_man_info.to_pickle(os.path.join(summary, "envinfo.pkl"))
-
-    if run_local:
-        par_var, data = setup.run(4)
-    else:
-        par_var, data = setup.remote(-1)
 
     par_var.to_pickle(os.path.join(summary, "metainfo.pkl"))
 
@@ -101,4 +131,4 @@ if __name__ == "__main__":
         os.path.join(summary, "time_95_informed.pkl")
     )
 
-    print("All simultion runs completed.")
+    print("All simulation runs completed.")
