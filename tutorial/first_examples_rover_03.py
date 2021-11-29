9d80f1019c832f49909db5be42d01b37c0f538c7
Mario Weidner <mario.weidner@hm.edu>
2021-11-29 11:33:46 +0100

    changed filepath

--- tutorial/first_examples_rover_03.py
+++ tutorial/first_examples_rover_03.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
                 "*.pNode[*].app[2].app.mapTypeLog": '"invSourceDist"',
                 "*.pNode[*].app[2].app.mapType": '"invSourceDist"'}},
         ],
-        output_path="/home/vm-sts/repos/crownet/analysis/suq-controller/tutorial/",
+        output_path=os.path.dirname(os.path.realpath(__file__)),
         output_folder=output_folder,
         repeat=1,
         seed_config={"sumo": "fixed", "omnet": "random"},
